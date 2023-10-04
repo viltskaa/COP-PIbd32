0b4d81a21c551e24e8f8a15bd7eb5232a86346d4
AnnZhimol <ankavanka731@gmail.com>
2023-10-04 17:03:08 +0400

    buisnesslogic

---
+++ Zhimol_lab_3/EnterpriseBusinessLogic/BusinessLogics/EmployeeLogic.cs
@@ -0,0 +1,66 @@
+﻿using EnterpriseContracts.BindingModels;
+using EnterpriseContracts.BusinessLogicContracts;
+using EnterpriseContracts.StorageContracts;
+using EnterpriseContracts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseBusinessLogic.BusinessLogics
+{
+    public class EmployeeLogic : IEmployeeLogic
+    {
+        private readonly IEmployeeStorage _empStorage;
+
+        public EmployeeLogic(IEmployeeStorage empStorage)
+        {
+            _empStorage = empStorage;
+        }
+
+        public void CreateOrUpdate(EmployeeBindingModel model)
+        {
+            var element = _empStorage.GetElement(
+                new EmployeeBindingModel
+                {
+                    FIO = model.FIO
+                });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Сотрудник с таким именем уже существует");
+            }
+            if (model.Id.HasValue)
+            {
+                _empStorage.Update(model);
+            }
+            else
+            {
+                _empStorage.Insert(model);
+            }
+        }
+
+        public void Delete(EmployeeBindingModel model)
+        {
+            var element = _empStorage.GetElement(new EmployeeBindingModel { Id = model.Id });
+            if (element == null)
+            {
+                throw new Exception("Сотрудник не найден");
+            }
+            _empStorage.Delete(model);
+        }
+
+        public List<EmployeeViewModel> Read(EmployeeBindingModel model)
+        {
+            if (model == null)
+            {
+                return _empStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<EmployeeViewModel> { _empStorage.GetElement(model) };
+            }
+            return _empStorage.GetFilteredList(model);
+        }
+    }
+}
---
+++ Zhimol_lab_3/EnterpriseBusinessLogic/BusinessLogics/SkillLogic.cs
@@ -0,0 +1,66 @@
+﻿using EnterpriseContracts.BindingModels;
+using EnterpriseContracts.BusinessLogicContracts;
+using EnterpriseContracts.StorageContracts;
+using EnterpriseContracts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseBusinessLogic.BusinessLogics
+{
+    public class SkillLogic : ISkillLogic
+    {
+        private readonly ISkillStorage _skillStorage;
+
+        public SkillLogic(ISkillStorage skillStorage)
+        {
+            _skillStorage = skillStorage;
+        }
+
+        public void CreateOrUpdate(SkillBindingModel model)
+        {
+            var element = _skillStorage.GetElement(
+                new SkillBindingModel
+                {
+                    Name = model.Name
+                });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Такой навык уже существует");
+            }
+            if (model.Id.HasValue)
+            {
+                _skillStorage.Update(model);
+            }
+            else
+            {
+                _skillStorage.Insert(model);
+            }
+        }
+
+        public void Delete(SkillBindingModel model)
+        {
+            var element = _skillStorage.GetElement(new SkillBindingModel { Id = model.Id });
+            if (element == null)
+            {
+                throw new Exception("Навык не найден");
+            }
+            _skillStorage.Delete(model);
+        }
+
+        public List<SkillViewModel> Read(SkillBindingModel model)
+        {
+            if (model == null)
+            {
+                return _skillStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<SkillViewModel> { _skillStorage.GetElement(model) };
+            }
+            return _skillStorage.GetFilteredList(model);
+        }
+    }
+}
