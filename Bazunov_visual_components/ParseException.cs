8785a649103c3fbab3809bea27fa7cc709c7cb05
Viltskaa <andrewstyou@gmail.com>
2023-10-04 12:37:32 +0400

    update files for nuget

--- Bazunov_visual_components/ParseException.cs
+++ Bazunov_visual_components/ParseException.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.DirectoryServices;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bazunov_VisualComponents
+﻿namespace Bazunov_VisualComponents
 {
     public class ParseException : Exception
     {
