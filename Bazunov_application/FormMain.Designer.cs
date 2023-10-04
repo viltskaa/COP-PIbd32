e26a42979b35f4d56253d7101d651b7c11828882
Viltskaa <andrewstyou@gmail.com>
2023-10-04 21:47:35 +0400

    start work

--- Bazunov_application/FormMain.Designer.cs
+++ Bazunov_application/FormMain.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxMany1 = new CustomComponent.ListBoxMany();
+            this.SuspendLayout();
+            // 
+            // listBoxMany1
+            // 
+            this.listBoxMany1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMany1.Location = new System.Drawing.Point(0, 0);
+            this.listBoxMany1.Name = "listBoxMany1";
+            this.listBoxMany1.SelectedIndex = -1;
+            this.listBoxMany1.Size = new System.Drawing.Size(738, 456);
+            this.listBoxMany1.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(750, 468);
+            this.Controls.Add(this.listBoxMany1);
+            this.Name = "FormMain";
+            this.Text = "Bazunov Application";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private CustomComponent.ListBoxMany listBoxMany1;
     }
 }
