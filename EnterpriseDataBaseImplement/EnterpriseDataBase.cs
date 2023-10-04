e8231a52c3535dbbb281356a2108ce7688d883c2
Viltskaa <andrewstyou@gmail.com>
2023-10-04 22:30:11 +0400

    init db

---
+++ EnterpriseDataBaseImplement/EnterpriseDataBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseDataBaseImplement;
+
+public class EnterpriseDataBase : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured == false) {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-VG5USAH\SQLEXPRESS;Initial Catalog=EnterpriseDatabase;Integrated Security=True;MultipleActiveResultSets=True; TrustServerCertificate=True");
+        }
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    //public virtual DbSet<Employee> Employees { set; get; }
+
+    //public virtual DbSet<Skill> Skills { set; get; }
+}
---
+++ EnterpriseDataBaseImplement/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnterpriseDataBaseImplement.Models;
+
+public class Employee
+{
+    [Required] public int Id { get; set; }
+    [Required] public string Subdivision { get; set; }
+    [Required] public string Fio { get; set; } = string.Empty;
+    [Required] public int Experience { get; set; }
+}
