b48640b912f5c8922481d9c494e90715557d98cc
Кашин Максим <kashin20031984@mail.ru>
2023-10-15 21:43:10 +0400

    АБАЛДЕТЬ РАБОТАЕТ ДОБАВЛЕНИЕ!!!

--- Employees/EmployeesView/FormEmployee.Designer.cs
+++ Employees/EmployeesView/FormEmployee.Designer.cs
@@ -30,12 +30,22 @@
         {
             this.LabelFIO = new System.Windows.Forms.Label();
             this.textBoxName = new System.Windows.Forms.TextBox();
+            this.dropDownList = new Kashin_1.DropDownList();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAutobiography = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LabelFIO
             // 
+            this.LabelFIO.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.LabelFIO.AutoSize = true;
-            this.LabelFIO.Location = new System.Drawing.Point(13, 12);
+            this.LabelFIO.Location = new System.Drawing.Point(13, 10);
             this.LabelFIO.Name = "LabelFIO";
             this.LabelFIO.Size = new System.Drawing.Size(34, 15);
             this.LabelFIO.TabIndex = 0;
@@ -43,19 +53,112 @@
             // 
             // textBoxName
             // 
-            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxName.Location = new System.Drawing.Point(13, 30);
+            this.textBoxName.Location = new System.Drawing.Point(13, 31);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(229, 23);
+            this.textBoxName.Size = new System.Drawing.Size(374, 23);
             this.textBoxName.TabIndex = 1;
+            // 
+            // dropDownList
+            // 
+            this.dropDownList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dropDownList.Location = new System.Drawing.Point(12, 81);
+            this.dropDownList.Name = "dropDownList";
+            this.dropDownList.SelectedValue = "";
+            this.dropDownList.Size = new System.Drawing.Size(374, 29);
+            this.dropDownList.TabIndex = 2;
+            this.dropDownList.Load += new System.EventHandler(this.FormEmployee_Load);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Должность";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Автобиография";
+            // 
+            // textBoxAutobiography
+            // 
+            this.textBoxAutobiography.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAutobiography.Location = new System.Drawing.Point(13, 131);
+            this.textBoxAutobiography.Name = "textBoxAutobiography";
+            this.textBoxAutobiography.Size = new System.Drawing.Size(374, 23);
+            this.textBoxAutobiography.TabIndex = 5;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker.Location = new System.Drawing.Point(13, 179);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(374, 23);
+            this.dateTimePicker.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Дата повышения квалификаций";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(13, 219);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(373, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(13, 248);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(373, 23);
+            this.buttonCancel.TabIndex = 9;
+            this.buttonCancel.Text = "Отменить";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // FormEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(254, 450);
+            this.ClientSize = new System.Drawing.Size(399, 287);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.textBoxAutobiography);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dropDownList);
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.LabelFIO);
             this.Name = "FormEmployee";
@@ -69,5 +172,13 @@
 
         private Label LabelFIO;
         private TextBox textBoxName;
+        private Kashin_1.DropDownList dropDownList;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxAutobiography;
+        private DateTimePicker dateTimePicker;
+        private Label label3;
+        private Button buttonSave;
+        private Button buttonCancel;
     }
 }
--- Employees/EmployeesView/FormEmployee.cs
+++ Employees/EmployeesView/FormEmployee.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EmployeesContracts.BindingModels;
+using EmployeesContracts.BusinessLogicsContracts;
+using EmployeesContracts.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,9 +16,92 @@ namespace EmployeesView
 {
     public partial class FormEmployee : Form
     {
-        public FormEmployee()
+        public int Id { set { id = value; } }
+
+        private readonly IEmployeeLogic _logic;
+        private readonly IPostLogic _logicS;
+
+        private int? id;
+
+        public FormEmployee(IEmployeeLogic logic, IPostLogic logicS)
         {
             InitializeComponent();
+            _logic = logic;
+            _logicS = logicS;
+        }
+
+        private void FormEmployee_Load(object sender, EventArgs e)
+        {
+            List<PostViewModel> viewS = _logicS.Read(null);
+            if (viewS != null)
+            {
+                foreach (PostViewModel s in viewS)
+                {
+                    dropDownList.Items.Add(s.Name);
+                }
+            }
+            if (id.HasValue)
+            {
+                try
+                {
+                    EmployeeViewModel view = _logic.Read(new EmployeeBindingModel { Id = id.Value })?[0];
+                    if (view != null)
+                    {
+                        textBoxAutobiography.Text = view.Autobiography;
+                        textBoxName.Text = view.Name;
+                        dropDownList.SelectedValue = view.Post;
+                        dateTimePicker.Value = (DateTime)view.Upgrade;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните имя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(dropDownList.SelectedValue))
+            {
+                MessageBox.Show("Выберите должность", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBoxAutobiography.Text == null)
+            {
+                MessageBox.Show("Заполните автобиографию", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                _logic.CreateOrUpdate(new EmployeeBindingModel
+                {
+                    Id = id,
+                    Autobiography = textBoxAutobiography.Text,
+                    Name = textBoxName.Text,
+                    Post = dropDownList.SelectedValue.ToString(),
+                    Upgrade = dateTimePicker.Value
+                });
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
     }
 }
